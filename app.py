38637c297bae1e7a84cd0a66ce383a52c8c8a522
Programer6 <delroyprithvi@gmail.com>
2025-08-17 10:43:05 +0200

    get & post

--- app.py
+++ app.py
@@ -2,15 +2,13 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
-
-
-@app.route("/greet")
-def greet():
-    name = request.args.get("name", "World")
-    return render_template("greet.html", name=name)
+    if request.method == "POST":
+            name = request.form.get("name", "World")
+            return render_template("greet.html", name=name)
+    else:
+        return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
