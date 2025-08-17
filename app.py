3ccaa1aad6153581368d4a7c3540d093d126ce52
Programer6 <delroyprithvi@gmail.com>
2025-08-17 06:56:54 +0200

    css to static

--- app.py
+++ app.py
@@ -5,5 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    name = request.args.get("name", "World")
-    return render_template("index.html", name=name)
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
